c8936393b9e15636d8e6cfdcfebbf6b49e161b03
imperative-scientist-4600 <nelom3@gmail.com>
2020-11-08 01:24:08 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,5 @@
 def turn_count(board)
   board.count {|a| a == "X" || a == "O"}
-  
-  # empty_cells = 0
-  # board.each do |cell|
-  #   if cell != 'X' && cell != 'O'
-  #     empty_cells += 1
-  #   end
-  # end
-  # board.length - empty_cells
 end
     
 def current_player(board)
